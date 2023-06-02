6e899fc302917a2216293db2914c23a3e2297f79
Rgomes3333 <raulgc268@outlook.com>
2023-06-02 13:07:47 -0300

    Consumindo API e realizando o mapeamento com o map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [pokemonList, setpokemonList] = useState([])
+
+  useEffect(() => {
+
+      axios.get('https://pokeapi.co/api/v2/pokemon')
+      .then((res) => {setpokemonList(res.data.results)})
+      .catch((err) => (err))
+
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Consumindo API e mapeando os dados, trazendo o nome dos pokemons:
+      <div>{pokemonList.map((pokemon) => (
+            <div>{pokemon.name}</div>
+        ))}</div>
+      
     </div>
   );
 }
